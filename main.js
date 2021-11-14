96761b043909c0e3425bdddfcd0cad5ab1e8749b
Erikisak <erik.isak@icloud.com>
2021-11-14 21:50:04 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,17 @@
+function promptMe() {
+    if (prompt ("Please enter the secret password (tip, where is R2D2)") == "May the force be with you"){
+        window.location.href = "./page2.html";
+    }
+}
+
+function pressR2d2() {
+    alert ("Not yet, find the stormtrooper");
+}
+
+function pressStorm() {
+    alert ("Password: May the force be with you");
+}
+
+function clickMe() {
+    prompt ("Hej")
+}
