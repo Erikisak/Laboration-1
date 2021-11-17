878903788da39e87979389dffcb82696cd3dfab6
Erikisak <erik.isak@icloud.com>
2021-11-17 13:07:28 +0100

    Yoda first function

--- page3.js
+++ page3.js
@@ -22,14 +22,32 @@ function addLuke() {
 function addObi() {
     const addObi = alert ("You have chosen Obi Wan Kenobi");
 }
+
 function addYoda() {
     const addYoda = alert ("You have chosen Master Yoda");
+    
+    const choice1 = prompt("Hello Master Yoda! Do you want to use your force or your lightsaber"); 
+    
+    confirm("You choose" + " " + choice1);
+    
+    if(choice1 === "force") {
+        const choice2 = prompt("How do you want to use your force? Attack or Block");
+        if(choice2 === "attack") 
+            alert("Vader gets pushed to the ground");
+        if( choice2 === "block")
+            alert("Vader attacks you but you manage to block him");
+        else if(choice2)
+            alert("Wrong choice, Vader defeated you");
+    }
+    if(choice1 === "lightsaber") {
+        const choice3 = prompt("How to do want to use your lightsaber? Attack or Block");
+        if(choice3 === "attack")
+            alert("You hit Vader with your lightsaber and cut his hand off");
+        if(choice3 === "block")
+            alert("Vader attacks you but you manage to block him");
+        else if(choice3)
+            alert("Wrong choice, Vader defeated you");
+    }
+
+
 }
-
-let text = document.getElementById("text");
-let option1 = document.getElementById("option1");
-let option2 = document.getElementById("option2");
-
-start();
-
-function start(); {}
