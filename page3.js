1201ecb5fc543ec19953f05ad1a6dd7de39e8cc3
Erikisak <erik.isak@icloud.com>
2021-11-18 09:40:01 +0100

    Edited alerts and prompts

--- page3.js
+++ page3.js
@@ -19,10 +19,14 @@ function addEventListeners() {
 function addLuke() {
     const addLuke = alert ("You have chosen Luke Skywalker");
 
-    
+    const choice1
+
+
 
 
 }
+
+
 function addObi() {
     const addObi = alert ("You have chosen Obi Wan Kenobi");
 
@@ -36,6 +40,8 @@ function addObi() {
             alert("Vader gets pushed away and you can plan a new attack");
         if(choice2 === "escape")
             alert("You tried to escape but Vader used his forced to hurt you instead");
+        
+            
     }
     if(choice1 === "lightsaber") {
         const choice2 = prompt("You chose lightsaber! Now choose to 'attack' or 'block' Vader's attack ");
@@ -48,7 +54,7 @@ function addObi() {
         if(choiceAgain === "attack again")
         alert("Vader cannot block you this time, he is defeated");
         if(choiceAgain === "use the force")
-        alert("You used the force, but Vader is to strong! You are defeated! Try another character")
+        alert("You used the force, but Vader is to strong! You are defeated! Try another character");
 
 
     }
@@ -60,41 +66,27 @@ function addObi() {
 function addYoda() {
     const addYoda = alert ("You have chosen Master Yoda");
     
-    const choice1 = prompt("Hello Master Yoda! Do you want to use your force or your lightsaber"); 
+    const choice1 = prompt("Hello Master Yoda! Do you want to use your 'force' or your 'lightsaber'"); 
     
     confirm("You choose" + " " + choice1);
     
     if(choice1 === "force") {
-        const choice2 = prompt("How do you want to use your force? Attack or Block");
+        const choice2 = prompt("How do you want to use your force? 'Attack' or 'Block'");
         
         if(choice2 === "attack") 
-            alert("Vader gets pushed to the ground");
+            alert("Good choice! Your attack is strong, too strong for Vader! He is defeated");
         
         if( choice2 === "block")
-            alert("Vader attacks you but you manage to block him");
-        
+            alert("You tried to block Vader but he is too strong! You are defeated. Try again or another character")
+  
+            
     }
     if(choice1 === "lightsaber") {
         const choice3 = prompt("How to do want to use your lightsaber? Attack or Block");
         if(choice3 === "attack")
-            alert("You hit Vader with your lightsaber and cut his hand off");
+            alert("You hit Vader with your lightsaber and cut his hand off, Vader escapes");
         if(choice3 === "block")
-            alert("Vader attacks you but you manage to block him");
+            alert("You have to attack Vader! He is to strong for your block, you are defeated. Try again or another character");
     }
-
-    const choice4 = prompt("Darth Vader is still alive, what do you want to do? Type in 'try new character' or 'attack again' ");
-    if(choice4 === "try new character")
-        confirm("Choose Obi-Wan or Luke to defeat Darth Vader");
-    
-    if(choice4 === "attack again") {
-        const choice5 = prompt("Vader is weak, how do you want to attack him? Type in 'force' or 'lightsaber'")
-            if(choice5 === "force")
-                alert("You grabbed Vaders lightsaber with the force, he is defeated");
-            if(choice5 === "lightsaber")
-                alert("You cut Vaders head off, he is defeated");
-    
-    }
-
-
 
 }
